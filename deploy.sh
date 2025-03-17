a6d7a7f5a997587a2d583bb02bbd729cf7a8bc73
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-03-17 06:00:24 +0200

    Auto-deploy latest versions

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf portfolio
-git clone https://github.com/reinisvaravs/portfolio.git
+# rm -rf portfolio
+# git clone https://github.com/reinisvaravs/portfolio.git
 
 rm -rf frontend-todo
 git clone https://github.com/reinisvaravs/frontend-todo.git
@@ -13,7 +13,7 @@ cd ..
 mkdir -p temp/frontend-todo
 mv frontend-todo/dist ./temp/frontend-todo
 rm -rf frontend-todo
-mv ./temp/frontend-todo ../server
+mv ./temp/frontend-todo ../reinisvaravs
 
 rm -rf frontend-ecom
 git clone https://github.com/reinisvaravs/frontend-ecom.git
@@ -25,7 +25,7 @@ cd ..
 mkdir -p temp/frontend-ecom
 mv frontend-ecom/dist ./temp/frontend-ecom
 rm -rf frontend-ecom
-mv ./temp/frontend-ecom ../server
+mv ./temp/frontend-ecom ../reinisvaravs
 rm -rf temp
 
 git add .
