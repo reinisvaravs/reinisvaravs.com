c4ada8cc5eee65f936d4d06d772c67993cd35541
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-04-08 12:35:09 +0300

    Auto-deploy latest versions

--- deploy.sh
+++ deploy.sh
@@ -14,20 +14,20 @@ mv portfolio/dist ./temp/portfolio
 rm -rf portfolio
 mv ./temp/portfolio ../reinisvaravs
 
-# echo "[ecom] Getting it"
-# rm -rf frontend-ecom
-# git clone https://github.com/reinisvaravs/frontend-ecom.git
-# cd frontend-ecom
-# npm install
-# npm run build
-# cd ..
+echo "[ecom] Getting it"
+rm -rf frontend-ecom
+git clone https://github.com/reinisvaravs/frontend-ecom.git
+cd frontend-ecom
+npm install
+npm run build
+cd ..
 
-# echo "[ecom] Placing it in temp/"
-# mkdir -p temp/frontend-ecom
-# mv frontend-ecom/dist ./temp/frontend-ecom
-# rm -rf frontend-ecom
-# mv ./temp/frontend-ecom ../reinisvaravs
-# rm -rf temp
+echo "[ecom] Placing it in temp/"
+mkdir -p temp/frontend-ecom
+mv frontend-ecom/dist ./temp/frontend-ecom
+rm -rf frontend-ecom
+mv ./temp/frontend-ecom ../reinisvaravs
+rm -rf temp
 
 echo "[server] Pushing changes"
 git add .
