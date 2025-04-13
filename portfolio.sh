050936e9ece479a7b3c8de6a7e129757cd49a060
reinisvaravs <reinisroberts.varavs2@gmail.com>
2025-04-13 03:37:51 +0300

    Auto-deploy latest versions

---
+++ portfolio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "[portfolio] Getting it"
+rm -rf portfolio
+git clone https://github.com/reinisvaravs/portfolio.git
+cd portfolio
+npm install
+npm run build
+cd ..
+
+echo "[portfolio] Placing it in temp/"
+mkdir -p temp/portfolio
+mv portfolio/dist ./temp/portfolio
+rm -rf portfolio
+mv ./temp/portfolio ../reinisvaravs
+
+echo "[server] Pushing changes"
+git add .
+git commit -m "Auto-deploy latest versions"
+git push origin main
+
+echo "✅✅✅ Deployment complete! [portfolio only]"
